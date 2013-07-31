87b3151699201995e4e15b42b1bac15cd467c2e9
Humberto <humberto04@gmail.com>
2013-07-31 10:14:20 -0700

    Team changed to team_name on Game API

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -34,7 +34,7 @@ class Api::GamesController < ActionController::Base
       team_stat.player_stats.each do |player_stat|
         player_stats << {:id => player_stat.id, :player_name => player_stat.player.first_name, :points => player_stat.points, :fouls => player_stat.fouls}
       end
-      team_stats << {:id => team_stat.id, :team => team_stat.team.name, :score => team_stat.score, :result => team_stat.result, :player_stats => player_stats}
+      team_stats << {:id => team_stat.id, :team_name => team_stat.team.name, :score => team_stat.score, :result => team_stat.result, :player_stats => player_stats}
     end
     list = {:game => { :id => game.id, :team_stats => team_stats }}
     puts "#{list.class}"
