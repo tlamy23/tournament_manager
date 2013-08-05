f8cd74b9a6b57609e2f0a2761140dc7e35b1b74b
Humberto <humberto04@gmail.com>
2013-08-05 14:37:08 -0700

    Dashboard updated with weight and height facts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
   def standings
+    @teams_weight = Team.all :select => 'teams.name, SUM(players.weight) as weight_sum',
+                             :joins => :players,
+                             :group => 'teams.id',
+                             :order => 'weight_sum DESC'
+
+    @teams_height = Team.all :select => 'teams.name, SUM(players.height) as height_sum',
+                             :joins => :players,
+                             :group => 'teams.id',
+                             :order => 'height_sum DESC'
   end
 end
