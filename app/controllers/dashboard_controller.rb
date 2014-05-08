d6d9b38ef92483296060c18f1527a8feb780844f
tlamy23 <tlamy23@hotmail.com>
2014-05-08 13:19:16 -0700

    add stats table

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,5 +9,23 @@ class DashboardController < ApplicationController
                              :joins => :players,
                              :group => 'teams.id',
                              :order => 'height_sum DESC'
+    @stats = Game.stats
   end
 end
+
+
+ #Game.joins(:team_stats => { :team => :team_seasons })
+ #    .select("team_stats.team_id as id,
+ #            teams.name as name,
+ #            team_seasons.id as team_season_id,
+ #            SUM(CASE WHEN team_stats.result = 'Won' THEN 1 ELSE 0 END) AS won,
+ #            SUM(CASE WHEN team_stats.result = 'Lost' THEN 1 ELSE 0 END) AS lost,
+ #            COUNT(*) as total")
+ #   .where('games.tournament_id = ?', self.id )
+ #   .where('team_seasons.tournament_id = ?', self.id )
+ #   .where('games.played = true')
+ #   .group('team_stats.team_id,
+ #           team_seasons.id,
+ #          teams.name,
+ #          games.tournament_id')
+ #   .order('won DESC')
--- app/models/game.rb
+++ app/models/game.rb
@@ -5,7 +5,26 @@ class Game < ActiveRecord::Base
   after_save :set_game_winner
 
   accepts_nested_attributes_for :team_stats
+  def self.win(team_id)
+    Game.joins(:team_stats).where(team_stats: {team_id: team_id, result:'Won'})
+  end
 
+  def self.lost(team_id)
+    Game.joins(:team_stats).where(team_stats: {team_id: team_id, result:'Lost'})
+  end
+
+  def self.stats
+    Schedule.joins(:games => {:team_stats => :team})
+       .select("team_stats.team_id as id,
+               teams.name,
+               SUM(CASE WHEN team_stats.result = 'Won' THEN 1 ELSE 0 END) AS won,
+               SUM(CASE WHEN team_stats.result = 'Lost' THEN 1 ELSE 0 END) AS lost,
+               COUNT(*) as total")
+      .where("schedules.date BETWEEN '2014-01-01' AND current_date")
+      .group('team_stats.team_id,
+             teams.name')
+      .order('won DESC, lost ASC')
+  end
   private
   def set_game_winner
     team_a = self.team_stats[0]
